e248fbccf0e71e69f9f65e522d9e9393035047c3
Patrik <patrikmatos.tecinfo@gmail.com>
2019-12-26 21:53:17 -0300

    Add BoxShadow in Wrapper

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -10,10 +10,12 @@ const Wrapper = styled('div')`
 const ContentWrapper = styled('div')`
   max-width: 700px;
   margin: auto;
+  box-shadow: -1px -1px 7px 0px rgba(0,0,0,0.50);
+  border-radius: 10px;
 `;
 
 const Content = styled('div')`
-
+  
 `;
 
 export default function Page(props) {
