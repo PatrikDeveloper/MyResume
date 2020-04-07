49f05bf79f29bfdc1e618b06e7ce123f69d0d50a
Patrik Matos <patrikmatos.tecinfo@gmail.com>
2020-04-07 17:06:02 -0300

    Update build

---
+++ docs/precache-manifest.c672b87f52e15acbb49a62b1cda30c45.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdb2b7a1bc9011ceaaffcd2758500ff5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5f325e4e543972f10c14",
+    "url": "./static/css/main.f946646c.chunk.css"
+  },
+  {
+    "revision": "2081756ca6684c532d02",
+    "url": "./static/js/2.17d50c94.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "./static/js/2.17d50c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f325e4e543972f10c14",
+    "url": "./static/js/main.881d06d7.chunk.js"
+  },
+  {
+    "revision": "53b9e9e56c690dbed2e9",
+    "url": "./static/js/runtime-main.ef4170b3.js"
+  },
+  {
+    "revision": "803668c2119d4a0c904aa3c050101a45",
+    "url": "./static/media/github.803668c2.png"
+  },
+  {
+    "revision": "f0233cc72bb653e7ecf264bf69d57496",
+    "url": "./static/media/header.f0233cc7.jpg"
+  },
+  {
+    "revision": "5c570427ee23f69853d28aec805eee79",
+    "url": "./static/media/instagram.5c570427.png"
+  },
+  {
+    "revision": "fbd8c42fea6b579be930647c4266884c",
+    "url": "./static/media/patrik.fbd8c42f.png"
+  }
+]);
